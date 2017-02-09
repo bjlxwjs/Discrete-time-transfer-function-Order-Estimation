9bd532c489e52d9d335a9e9ee62128d0553b8517
bjlxwjs <bjlxwjs@163.com>
2017-02-09 19:49:51 +0800

    Create example.m

---
+++ example.m
@@ -0,0 +1,13 @@
+num=[1.2 0.3];
+den=[1 1.2 0.7 0.2];
+t= 0:1/1000:2;
+Ts=0.001;
+G=tf(num,den,Ts);
+
+  %              1.2 z + 0.3
+  % G(Z)= ---------------------------
+  %        z^3 + 1.2 z^2 + 0.7 z + 0.2
+
+x = chirp(t,0,1,250);  
+[y,t]=lsim(G,x,t);
+true_order=order_estimation( y , nn )
